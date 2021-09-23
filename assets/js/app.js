4b288af3705dc6c6a9037de64138eeb71e62c363
Rodrigo Guedes da Silva <79825051+RodrigoGuedesdaSilva@users.noreply.github.com>
2021-09-23 19:45:44 -0300

    Added Dellas Website Files

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+/* Funcionalidades do Menu */
+
+const menu = document.querySelector('.hamburger-menu');
+const menuLink = document.querySelector('.navbar-nav');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('active');
+    menuLink.classList.toggle('is-active');
+})
+
+document.onclick = function(e) {
+    if(e.target !== menu && e.target !== menuLink) {
+        menu.classList.remove('active');
+        menuLink.classList.remove('is-active')
+    }
+}
+
+/* Funcionalidades do Filtro da Seção Coleções */
+
+var $grid = $('.colecao-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on('click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({filter:filterValue});
+  });
+
+  var filterBtns = $('.filter-button-group').find('button');
+  function resetFilterBtns() {
+      filterBtns.each(function() {
+          $(this).removeClass('active-filter-btn');
+      });
+  }
