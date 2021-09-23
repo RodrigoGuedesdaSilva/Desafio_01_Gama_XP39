ed1fcc53b49154c67f3e50037d0a2ecfc94c38d9
Rodrigo Guedes da Silva <79825051+RodrigoGuedesdaSilva@users.noreply.github.com>
2021-09-23 19:55:41 -0300

    Removing onclick function

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,13 +7,13 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('active');
     menuLink.classList.toggle('is-active');
 })
-
+/*
 document.onclick = function(e) {
     if(e.target !== menu && e.target !== menuLink) {
         menu.classList.remove('active');
         menuLink.classList.remove('is-active')
     }
-}
+}*/
 
 /* Funcionalidades do Filtro da Seção Coleções */
 
